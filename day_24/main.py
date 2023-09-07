b5630d291c857c207ada6ea217a65a2420c98409
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-09-08 01:19:08 +0530

    learning file management in pyton

--- day_24/main.py
+++ day_24/main.py
@@ -1,15 +1,13 @@
-with open("C:\ Users\HP\Desktop\ 100 days of python\ 100daysofpython\day_24\input\letters\starting_names.txt",
-          'r') as format_file:
+with open("./input/letters/starting_names.txt", 'r') as format_file:
     format_for_letter = format_file.read()
 
-with open("C:\ Users\HP\Desktop\ 100 days of python\ 100daysofpython\day_24\input\ names\invited_names.txt",
-          'r') as names:
-    names_to_be_added = names.read()
-
-replacing_name = names_to_be_added.split()
+with open("./input/names/invited_names.txt", 'r') as names:
+    names_to_be_added = names.read().strip()
+    replacing_name = names_to_be_added.split()
 
 for word in replacing_name:
+    print(word)
     formatted = format_for_letter
     formatted = formatted.replace("[name]", word)
-    with open(f"/output/ready to share/letter_for_{word}.docx", 'w') as output:
+    with open(f"./output/ready to share/letter_for_{word}.docx", 'w') as output:
         output.write(formatted)
