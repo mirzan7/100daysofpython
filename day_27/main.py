73d0c10a42d83e1f4b6665e1914727d724624c55
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-09-16 20:23:10 +0530

    learning tkinter in pyton

--- day_27/main.py
+++ day_27/main.py
@@ -3,7 +3,21 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first tkinter program")
 window.minsize(500, 300)
-text_label = tkinter.Label(text= "my name is mirzan")
+text_label = tkinter.Label(text="my name is mirzan")
 text_label.pack()
+
+
+def clicked():
+    user_input = enter_input.get()
+    text_label["text"] = user_input
+
+
+enter_input = tkinter.Entry(width=50)
+
+button = tkinter.Button(text="click me", command=clicked)
+
+button.pack()
+enter_input.pack()
+
 
 window.mainloop()
---
+++ day_27/playground.py
@@ -0,0 +1,14 @@
+import tkinter
+
+
+def add(*numbers):
+    sum_of_number = 0
+    for number in numbers:
+        sum_of_number += number
+    return sum_of_number
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+number5 = int(input())
