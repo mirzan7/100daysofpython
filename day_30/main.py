1ef83d46960f7927c97559b4c22ab89278ed2708
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-09-26 23:10:44 +0530

    ERROR HANDLING

---
+++ day_30/main.py
@@ -0,0 +1,25 @@
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["sfdgg"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("something    !!!!!")
+# except KeyError as error_message:
+#     print("the key is not in the dictionary!!!!!!")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+# height = float(input("height:   "))
+# weight = int(input("weight:    "))
+# if height>
+
+fruits = ["apple", "pear", "orange"]
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit+" pie")
+
+make_pie(4)
+if
