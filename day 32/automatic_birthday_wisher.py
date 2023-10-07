e6285de65bae406281044901e1a95c38871232fc
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-10-07 15:52:53 +0530

    learning smtp modules

---
+++ day 32/automatic_birthday_wisher.py
@@ -0,0 +1,1 @@
+pthoonanywhere.com
--- day 32/main.py
+++ day 32/main.py
@@ -1,15 +1,31 @@
 # import smtplib
 # my_email = "mirzanmiraj@yahoo.com"
-# passwrd = "AYSHAMANZIL "
+# passwrd = "1234567889*"
 # connection = smtplib.SMTP("smtp.mail.yahoo.com")
 # connection.starttls()
 # connection.login(user=my_email, password=passwrd)
 # connection.sendmail(from_addr=my_email, to_addrs="mirzan.testing@gmail.com", msg="hello")
-# connection.close()
-
+# # connection.close()
+#
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+#
+# print(year)
+import random
+import smtplib
 import datetime as dt
-
 now = dt.datetime.now()
-year = now.year
-
-print(year)
+weekday = now.weekday()
+print(weekday)
+if weekday == 5:
+    with open("quotes.txt", 'r') as datafile:
+        quotes = datafile.readline()
+        random_quote = random.choice(quotes)
+    my_email = "mirzanmiraj@yahoo.com"
+    passwrd = "AYSHAMANZIL"
+    connection = smtplib.SMTP("smtp.mail.yahoo.com")
+    connection.starttls()
+    connection.login(user=my_email, password=passwrd)
+    connection.sendmail(from_addr=my_email, to_addrs="npmirzanmiraj5@gmail.com", msg=quotes)
