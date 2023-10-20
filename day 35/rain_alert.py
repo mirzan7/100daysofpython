3c5d3e58e1d8740cf50663fa338cffef8ae46dbe
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-10-20 23:33:18 +0530

    weather api and sms

--- day 35/rain_alert.py
+++ day 35/rain_alert.py
@@ -3,11 +3,12 @@ import requests
 api_key = "f4c266e099a12f1bc1df0b5a1283be51"
 
 parameters = {
-    "lat": 11.9850509056689,
-    "lon": 75.5559240883433,
-    "appid": "f4c266e099a12f1bc1df0b5a1283be51",
+    "lat": 33.44,
+    "lon": -94.04,
+    "exclude": "hourly",
+    "appid": api_key,
 }
 
-response = requests.get(url="", params=parameters)
+response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall", params=parameters)
 response.raise_for_status()
 print(response.json)
