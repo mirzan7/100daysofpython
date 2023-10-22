c1ede6691b2b98ca0c2b25a2a98fcd3f1bccd573
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-10-22 20:31:30 +0530

    weather api and sms

--- day 35/rain_alert.py
+++ day 35/rain_alert.py
@@ -1,14 +1,32 @@
 import requests
-
+from twilio.rest import Client
+will_rain = None
 api_key = "f4c266e099a12f1bc1df0b5a1283be51"
+account_sid = 'ACfe03f23f7b4709de8e7368889b3ff7ac'
+auth_token = '[AuthToken]'
 
 parameters = {
-    "lat": 33.44,
-    "lon": -94.04,
-    "exclude": "hourly",
-    "appid": api_key,
+    "lat": 13.08784,
+    "lon": 80.27847,
+    "APPID": api_key,
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall", params=parameters)
+response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameters)
 response.raise_for_status()
+weather_data = response.json
 print(response.json)
+weather_slice = weather_data["hourly"][:12]
+print(weather_slice)
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code)<700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="donot forget your umbrella today:)",
+        from_='+14695138970',
+        to='+91828*****'
+    )
+    print(message.status)
+
