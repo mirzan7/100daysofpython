9364db8b48f4e5cf6face6cd5b2a7b37e0c09e6b
Mirzan NP <npmirzanmiraj5@gmail.com>
2023-11-29 19:27:20 +0530

    web scrapping

--- day 45/main.py
+++ day 45/main.py
@@ -1,1 +1,13 @@
 from bs4 import BeautifulSoup
+with open("index.html") as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, "html.parser")
+all_of_the_anchor_tag = soup.find_all(name="a")
+# # print(soup.prettify())
+# for tag in all_of_the_anchor_tag:
+#     print(tag.get("href"))
+
+header = soup.find(name="span")
+print(header)
+
